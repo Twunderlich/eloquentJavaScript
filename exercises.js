828efbc1c012e32c849927ca184a66cf1e33a289
twunderlich <wunderlich.travis@gmail.com>
2018-02-25 22:13:46 -0600

    completed ch 5 HOF mother-child age diff, historical life expetancy

--- exercises.js
+++ exercises.js
@@ -391,7 +391,7 @@ function deepEqual( valueOne, valueTwo ) {
 // console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // // → true
 
-// chapter 5 - Flattening - incomplete
+// chapter 5 - Flattening - complete!
 
 //Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has 
 // all the elements of the input arrays.
@@ -404,14 +404,18 @@ function flatten( arr ) {
 	}, [] );
 }
 
-console.log( flatten( arrays ) );
+// console.log( flatten( arrays ) );
 // → [1, 2, 3, 4, 5, 6]
 
-// chapter 5 - Mother-child age difference  - incomplete
+// chapter 5 - Mother-child age difference  - complete!
 
-require("./path/to/ancestry.js") will get you the array.
-if (typeof module != "undefined" && module.exports)
-  module.exports = ANCESTRY_FILE;
+// required dataset not working. completed on website.
+
+//
+// require("./lib/ancestry.js")
+// if (typeof module != "undefined" && module.exports) {
+//   module.exports = ANCESTRY_FILE;
+// }
 
 // Using the example data set from this chapter, compute the average age difference between mothers and children (the age of 
 // the mother when the child is born). You can use the average function defined earlier in this chapter.
@@ -424,29 +428,62 @@ function average(array) {
   return array.reduce(plus) / array.length;
 }
 
-var byName = {};
-ancestry.forEach(function(person) {
-  byName[person.name] = person;
-});
+// var byName = {};
+// ancestry.forEach(function(person) {
+//   byName[person.name] = person;
+// });
 
-// Your code here.
+function isInArray( moth ) {
+  return byName[ moth ] !== undefined;
+};
+
+function avgAgeOfBirths( obj ) {
+  	var agesArray = [];
+	for ( var person in obj ) {
+      var per = obj[ person ];
+      if ( isInArray( per.mother ) ) {
+        agesArray.push( per.born - byName[ per.mother ].born )
+      }
+    }
+  return average( agesArray ).toFixed( 1 );
+}
+
+// console.log( avgAgeOfBirths( byName ) );
 
 // → 31.2
 
-// chapter 5 - Historical life expectancy - incomplete
+// chapter 5 - Historical life expectancy - complete!
 //
 // When we looked up all the people in our data set that lived more than 90 years, only the latest generation in the data came out. 
 // Let’s take a closer look at that phenomenon.
 //
 // Compute and output the average age of the people in the ancestry data set per century. A person is assigned to a century by 
-// taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100).
+// taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100).g
 
 function average(array) {
   function plus(a, b) { return a + b; }
   return array.reduce(plus) / array.length;
 }
 
-// Your code here.
+function avgAgePerCentury( arr ) {
+	var centuries = arr.reduce( function ( obj, person ) {
+      var century = Math.ceil( person.died / 100 );
+      if ( obj[ century ] === undefined ) {
+        obj[ century ] = ( person.died - person.born );
+      } else {
+      	obj[ century ] = ( obj[ century ] + ( person.died - person.born ) ) / 2;
+      }
+      return obj;
+    }, {} );
+  
+	for ( var year in centuries ) {
+      	var avgAge = centuries[ year ];
+      	if ( !Number.isInteger( avgAge ) ) {
+ 			avgAge = avgAge.toFixed( 1 );
+        }
+    	console.log( year + ': ' + avgAge );
+    }
+}
 
 // → 16: 43.5
 //   17: 51.2
